f7918695bf07d56e79a56e2b7cf723557df8e8b6
Sumopique <128811665+Sumopique@users.noreply.github.com>
2023-03-31 02:47:51 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+    
+        <title>Emijay </title>
+    
+    <style>
+        .cont {
+            width: 100%;
+            
+        } 
+        .header {
+            width: 100%;
+            height:  35px;
+            background-color: cadetblue;
+            color:white;
+            padding: 4% 4% 4% 4%;
+            text-align: center
+            
+            
+        }
+        
+    </style>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        echo "Emijay Global Resources & Service";
+        ?>
+        <h1></h1>
+        <div class="cont">
+          
+                <div class="header">
+                    <h1 class="">Click on the link to contact Emijay</h1>
+            </div>
+        </div>
+            
+              <div class="row bg-info">
+                <div class="col m-6">
+                    <p class="text-light p-3">The COM++ caughtb an access violation at <br />
+                    address 0x00213CD4 within method SaveCompleted  <br />of interfaceIPersistFile. The method attempted to access address 0x00000978</p>
+                    <div class=""><b><a href="wa.link/0vxk8l"> Contact Us</a></b></div>
+        </div> 
+              </div>
+                
+            
+            
+        </div>
+        
+           <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    </body>
+</html>
